30ce45f96216182ed8f79ea39d603f431f130839
Martins Ngene <martinsngene@gmail.com>
2022-01-26 13:11:11 +0100

    [update]: Built scrollable section

--- components/Test.jsx
+++ components/Test.jsx
@@ -31,37 +31,53 @@ const Test = () => {
   //   };
   // }, []);
 
+//   const button = document.getElementById('slide');
+
+// button.onclick = function () {
+//   document.getElementById('container3').scrollLeft += 20;
+// };
+
   return (
-    <div>
-      <div ref={ref} className="outer">
-        <Card />
+    <div className="relative">
+     <div className="absolute top-[-9.5%] w-[6rem] right-[2%] flex items-center justify-between">
+        <button onClick={function () {
+  const cardBox = document.getElementById('container3')
+  cardBox.scrollLeft += -400;
+  // cardBox.scrollBehavior = "smooth";
+}
+}>
+          <BsArrowLeftSquare size={"35px"} />
+        </button>
 
-        <Card />
-
-        <Card />
-
-        <Card />
-
-        <Card />
-
-        <Card />
-
-        <Card />
-
-        <Card />
-
-        <Card />
-
-        <Card />
+        <button id="slide" onClick={function () {
+  const cardBox = document.getElementById('container3')
+  cardBox.scrollLeft += 400;
+  // cardBox.scrollBehavior = "smooth";
+}
+}>
+          <BsArrowRightSquare size={"35px"} />
+        </button>
       </div>
+      <div id="container3" style={{scrollBehavior: "smooth"}} ref={ref} className="outer">
+        <Card />
 
-      <div>
-        <button onClick={scrollSection}>
-          <BsArrowLeftSquare size={"50px"} />
-        </button>
-        <button onClick={scrollSection}>
-          <BsArrowRightSquare size={"50px"} />
-        </button>
+        <Card />
+
+        <Card />
+
+        <Card />
+
+        <Card />
+
+        <Card />
+
+        <Card />
+
+        <Card />
+
+        <Card />
+
+        <Card />
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 import Test from "../components/Test";
+import HeroCarousel from "../components/HeroCarousel";
+import DisplayCard from "../components/DisplayCard";
 // import styles from "../styles/Home.module.css";
 // import dynamic from "next/dynamic";
 
@@ -12,6 +14,12 @@ export default function Home() {
   return (
     <div>
       <NavBar />
+      <HeroCarousel/>
+      <div className="flex items-center justify-center p-6">
+        <div className="w-[75%]">
+          <DisplayCard/>
+        </div>
+      </div>
       <Test />
       {/* <DynamicComponentWithNoSSR /> */}
       <Footer />
