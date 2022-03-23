ba8ec09a54f7cbf9794b639b58a554aaf1b3df16
Martins Ngene <martinsngene@gmail.com>
2022-03-23 16:37:50 +0100

    [update]

--- components/Card2.jsx
+++ components/Card2.jsx
@@ -6,7 +6,7 @@ const Card2 = ({ children, src, buttonLabel, link }) => {
   return (
     <div className="bg-[#fff] p-2 hover:shadow-2xl border border-[#e3e3e3] w-[300px]">
       <div>
-        <Image src={src} width={300} height={200} />
+        <Image src={src} width={300} height={200} alt="" />
       </div>
       <div className="cardDescriptionContainer p-2">
         <p>{children}</p>
--- components/CustomLink.jsx
+++ components/CustomLink.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const CustomLink = ({ children, className, onClick, textStyle, href }) => {
   return (
     <div className={className} onClick={onClick}>
-      <Link href={`${href}`}>
+      <Link href={`${href}`} passHref={true}>
         <span className={textStyle}>{children}</span>
       </Link>
     </div>
